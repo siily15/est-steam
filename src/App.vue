ec9c5b1fc82800aa91dafd3548affbe237ae0a7c
siily <arnokosk15@gmail.com>
2020-09-22 09:43:05 +0300

    new codes

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Primarynav />
+  <Secendarynav />
+</template>
+
+<script>
+import Primarynav from "./components/Primarynav.vue";
+import Secendarynav from "./components/Secendarynav.vue";
+
+export default {
+  name: "App",
+  components: {
+    Primarynav,
+    Secendarynav,
+  },
+};
+</script>
